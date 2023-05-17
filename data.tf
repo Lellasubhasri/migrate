0f9ce5a245b575236757ad02f55be21637674c84
lella subhasri <subhasri.lella@cloudreach.com>
2023-05-17 15:04:44 +0530

    internet facing lb added

--- data.tf
+++ data.tf
@@ -1,9 +1,9 @@
-# data "aws_route53_zone" "route53" {
-#   name         = "subhasri.aws.crlabs.cloud"
-#   private_zone = false  
-# }
+data "aws_route53_zone" "route53" {
+  name         = "subhasri.aws.crlabs.cloud"
+  private_zone = false  
+}
 
 
-# data "aws_availability_zones" "available" {
-#   state = "available" 
-# }
+data "aws_availability_zones" "available" {
+  state = "available" 
+}
