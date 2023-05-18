6bf30d5816017b7864f9a4b1dcca9b2de3bf6934
lella subhasri <subhasri.lella@cloudreach.com>
2023-05-18 18:21:24 +0530

    6

--- locals.tf
+++ locals.tf
@@ -1,14 +1,14 @@
-locals {
-  availability_zones      = slice(data.aws_availability_zones.available.names, 0, var.number_of_azs)
+# locals {
+#   availability_zones      = slice(data.aws_availability_zones.available.names, 0, var.number_of_azs)
 
-  public_subnet_cidr      = cidrsubnet(var.vpc_cidr, 6, 4)
-  private_subnet_cidr     = cidrsubnet(var.vpc_cidr, 7, 10)
-  #application_subnet_cidr = cidrsubnet(local.private_subnet_cidr, 2, 0)
-  database_subnet_cidr    = cidrsubnet(var.vpc_cidr, 7, 13)
-  #staging_area_subnet_cidr = cidrsubnet(local.private_subnet_cidr,2, 2)
+#   public_subnet_cidr      = cidrsubnet(var.vpc_cidr, 6, 4)
+#   private_subnet_cidr     = cidrsubnet(var.vpc_cidr, 7, 10)
+#   #application_subnet_cidr = cidrsubnet(local.private_subnet_cidr, 2, 0)
+#   database_subnet_cidr    = cidrsubnet(var.vpc_cidr, 7, 13)
+#   #staging_area_subnet_cidr = cidrsubnet(local.private_subnet_cidr,2, 2)
 
-  private_random_ip = "192.168.0.235"
+#   private_random_ip = "192.168.0.235"
 
-#   vpc_cidr = "10.0.0.0/16"
-# number_of_azs = "2"
-}
+# #   vpc_cidr = "10.0.0.0/16"
+# # number_of_azs = "2"
+# }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
-variable "vpc_cidr" {
-    type = string
-    description = "this is cidr address"
-}
+# variable "vpc_cidr" {
+#     type = string
+#     description = "this is cidr address"
+# }
 
-variable "number_of_azs" {
-    description = "this is for number of AZs to use"
-    type = number
-}
+# variable "number_of_azs" {
+#     description = "this is for number of AZs to use"
+#     type = number
+# }
