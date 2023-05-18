17c505f20d0979daeccd13781ac914455bf20321
lella subhasri <subhasri.lella@cloudreach.com>
2023-05-18 18:26:26 +0530

    message

--- route53.tf
+++ route53.tf
@@ -1,16 +1,16 @@
-# resource "aws_route53_zone" "nsrec" {
-#   name = "subhasri.aws.crlabs.cloud"
+resource "aws_route53_zone" "nsrec" {
+  name = "subhasri.aws.crlabs.cloud"
 
-#   tags = {
-#     Environment = "dev"
-#   }
-# }
+  tags = {
+    Environment = "dev"
+  }
+}
 
-# resource "aws_route53_record" "privatea_rec" {
-#   zone_id = aws_route53_zone.nsrec.zone_id
-#   name    = "resolve-test"
-#   type    = "A"
-#   ttl     = 300
-#   records = [local.private_random_ip]
+resource "aws_route53_record" "privatea_rec" {
+  zone_id = aws_route53_zone.nsrec.zone_id
+  name    = "resolve-test"
+  type    = "A"
+  ttl     = 300
+  records = [local.private_random_ip]
   
-# }
+}
